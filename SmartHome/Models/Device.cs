ada8b83ace8d094f921dea4774d668296a21d3f6
desmondtayweechuan <1602049@sit.singaporetech.edu.sg>
2018-02-08 20:12:02 +0800

    Device Model

---
+++ SmartHome/Models/Device.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    public class Device
+    {
+        private int DeviceID;
+        private int HouseholdID;
+        private String DeviceName;
+        private String Brand;
+        private String Model;
+        private String Type;
+        private String Location;
+        private String State;
+        private double UsageKwH;
+        private int favourite;
+        private DateTime timestamp;
+
+        public Device(int DeviceID, int HouseholdID, String DeviceName,
+            String Brand, String Model, String Type, String Location,
+            String State, double UsageKwH, int favourite, DateTime timestamp)
+        {
+            this.DeviceID = DeviceID;
+            this.HouseholdID = HouseholdID;
+            this.DeviceName = DeviceName;
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Type = Type;
+            this.Location = Location;
+            this.State = State;
+            this.UsageKwH = UsageKwH;
+            this.favourite = favourite;
+            this.timestamp = timestamp;
+        }
+
+
+
+
+    }
+}
