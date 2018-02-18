adffbeabdb1789283cb56966476cd26f25f40cde
desmondtayweechuan <1602049@sit.singaporetech.edu.sg>
2018-02-18 13:32:53 +0800

    Minor Bug Fix
    -Removed Nav header
    -Error meesage for Create
    -Remove some useless code in Device Controller

--- SmartHome/Controllers/DeviceController.cs
+++ SmartHome/Controllers/DeviceController.cs
@@ -16,43 +16,14 @@ namespace SmartHome.Controllers
 
         public IActionResult Index()
         {
-            //Device t = new Device("1", "1", "1","1");
-            //var device1 = new Device();
-            //model.Add(device1);
-            /*
-            var tour1 = new Tour();
-            tour1.Name = "Cali Walking Tour";
-
-            var tour2 = new Tour();
-            tour2.Name = "Cali Cycling Tour";
-
-            //var model = new List<Tour> { tour1, tour2 };
-            model.Add(tour1);
-            model.Add(tour2);
-            return View(model);
-            */
-            //model.Add(t);
             return View(model);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
 
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult SelectType()
         {
             ViewData["Message"] = "Your create page.";
-
             return View();
         }
 
@@ -73,7 +44,6 @@ namespace SmartHome.Controllers
 
             try
             {
-                // TODO: Add insert logic here
 
                 model.Add(device);
                 ViewData["Message"] = param;
@@ -172,8 +142,6 @@ namespace SmartHome.Controllers
             model.Remove(itemToRemove);
             return RedirectToAction(nameof(Index));
         }
-
-
 
 
 
