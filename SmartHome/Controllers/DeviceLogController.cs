22e21c56b74cf4a95e09be574fe5fe65074458f6
Shahrul Nizam <1602602@SIT.Singaporetech.edu.sg>
2018-03-22 17:06:41 +0800

    Added latest project

---
+++ SmartHome/Controllers/DeviceLogController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using Excel = Microsoft.Office.Interop.Excel;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using Microsoft.AspNetCore.Hosting;
+using OfficeOpenXml;
+using SmartHome.Models;
+using SmartHome.Utility;
+
+
+namespace SmartHome.Controllers
+{
+    public class DeviceLogController : Controller
+    {
+        static List<DeviceLog> logList = new List<DeviceLog>();
+		private readonly IHostingEnvironment _hostingEnvironment;
+        
+        List<DeviceLog> dataList = new List<DeviceLog>();
+		
+		public DeviceLogController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+		
+        public ActionResult Index()
+        {
+            // Dummy data
+            var log1 = new DeviceLog(1, "Mitusubishi Aircon", "Bedroom", "Air Con", "ON",0);
+            var log2 = new DeviceLog(2, "Mitusubishi Aircon", "Bedroom", "Air con", "OFF", 120.0);
+            var log3 = new DeviceLog(3, "Toshiba Fan", "Living room", "Fan", "ON", 0);
+            var log4 = new DeviceLog(4, "Toshiba fan ", "Living room", "Fan", "OFF", 100.0);
+            var log5 = new DeviceLog(5, "Led light", "Kitchen", "Light", "ON", 0);
+            var log6 = new DeviceLog(6, "Led light", "Kitchen", "Light", "OFF", 200.0);
+
+
+            var logList = new List<DeviceLog> { log1,log2,log3,log4,log5,log6 };
+           
+           
+
+            return View(logList);
+        }
+
+        public IActionResult LogManagement()
+        {
+            return View();
+        }
+
+		public ActionResult UploadToDB()
+        {
+            return View();
+        }
+		
+        [HttpPost] 
+        [Route("UploadToDB")]
+        public ActionResult UploadToDB(string path) 
+        {
+            System.Diagnostics.Debug.WriteLine("Path = " + path);
+            var filePath = path;
+            FileInfo file = new FileInfo(filePath);
+
+            // Can use for csv
+            //using (StreamReader sr = file.OpenText())
+            //{
+            //    string s = "";
+            //    while ((s = sr.ReadLine()) != null)
+            //    {
+            //        System.Diagnostics.Debug.WriteLine("MESSAGESSSSSSSSSSSS = " + s);
+            //        Console.WriteLine(s);
+            //    }
+            //}
+
+            // For xlsx
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+                var totalRows = myWorksheet.Dimension.End.Row;
+                var totalColumns = myWorksheet.Dimension.End.Column;
+
+                
+                for (int rowNum = 1; rowNum <= totalRows; rowNum++) //selet starting row here
+                {
+                    var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                    var sb = new StringBuilder(); //this is your your data
+                    sb.AppendLine(string.Join(",", row));
+                    string[] tokens = sb.ToString().Split(",");
+                    DeviceLog dataset = new DeviceLog();
+                    dataset.Id = Convert.ToInt32(tokens[0]);
+                    dataset.householdId = Convert.ToInt32(tokens[1]);
+                    dataset.name = tokens[2];
+                    dataset.location = tokens[3];
+                    dataset.type = tokens[4];
+                    dataset.state = tokens[5];
+                    dataset.kWh = Convert.ToDouble(tokens[6]);
+                    dataset.dateTime = DateTime.Parse(tokens[7]);
+                    //dataset.dateTime = DateTime.ParseExact(tokens[7], "dd/MM/yyyy h:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture);
+                    dataList.Add(dataset);
+                }
+            }
+            return View(dataList);
+
+            
+        }
+
+        public IActionResult ExportFile()
+        {
+            
+            return View();
+        }
+
+        // Export log
+        [HttpGet]
+        public FileContentResult ExportToExcel()
+        {
+            //List<DeviceLog> technologies = TempData["test"];'
+            var log1 = new DeviceLog(1, "Mitusubishi Aircon", "Bedroom", "Air Con", "ON", 0);
+            var log2 = new DeviceLog(2, "Mitusubishi Aircon", "Bedroom", "Air con", "OFF", 120.0);
+            var log3 = new DeviceLog(3, "Toshiba Fan", "Living room", "Fan", "ON", 0);
+            var log4 = new DeviceLog(4, "Toshiba fan ", "Living room", "Fan", "OFF", 100.0);
+            var log5 = new DeviceLog(5, "Led light", "Kitchen", "Light", "ON", 0);
+            var log6 = new DeviceLog(6, "Led light", "Kitchen", "Light", "OFF", 200.0);
+
+
+            List<DeviceLog> logList = new List<DeviceLog> { log1, log2, log3, log4, log5, log6 };
+
+            string[] columns = { "name", "location", "type","state","kWh","dateTime"};
+            byte[] filecontent = ExcelExportHelper.ExportExcel(logList, "Device Log", true, columns);
+            String date = DateTime.Now.ToString("dd-MM-yyyy");
+            return File(filecontent, ExcelExportHelper.ExcelContentType, "DeviceLog-"+date+".xlsx");
+        }
+
+        public IActionResult SortLogs()
+        {
+            return View();
+        }
+    }
+}
---
+++ SmartHome/Models/Light.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models
+{
+    public class Light : DeviceLog
+    {
+
+        private int brightness { get; set; }
+        private int colorTemperature { get; set; }
+
+      
+    }
+}
