a3bf234eb4116047244286b20e9a210e6913ceff
DES <skypi205@gmail.com>
2018-03-29 16:39:28 +0800

    restructure

---
+++ SmartHome/Models/SmartDevice/SmartFan/SmartFan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHome.Models.SmartDevice.SmartFan
+{
+    public class SmartFan : ISmartDevice
+    {
+    }
+}
