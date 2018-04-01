7eb65df4011ad4ac935f5ba87aa1be3bdcbac750
ernestboey <ernestboeywz@gmail.com>
2018-04-01 18:44:33 +0800

    Added testing code for mongodb C# implementation (removing later)

--- SmartHome/DAL/UnitOfWork/UnitOfWork.cs
+++ SmartHome/DAL/UnitOfWork/UnitOfWork.cs
@@ -22,25 +22,41 @@ namespace SmartHome.DAL.UnitOfWork
         // append query to Queries
         public void RegisterNew(IQueryable<T> createQuery)
         {
+            // .InsertOne()
             throw new NotImplementedException();
         }
 
         public void RegisterDirty(IQueryable<T> updateQuery)
         {
+            // .UpdateMany()
+            // var result = collection.UpdateOne(filter, update);
+
+//            if (result.IsModifiedCountAvailable)
+//            {
+//                Console.WriteLine(result.ModifiedCount);
+//            }
             throw new NotImplementedException();
         }
 
         public void RegisterDeleted(IQueryable<T> deleteQuery)
         {
+            // .DeleteMany()
             throw new NotImplementedException();
         }
-        
+
         // Retrieve document based on the retrieve query
-        BsonDocument IUnitOfWork<T>.ExecuteSelection(IQueryable<T> retrieveQuery)
+        public List<BsonDocument> ExecuteRetrieveAll(IQueryable<T> retrieveQuery)
         {
             // execute query, store result in var
             // convert var to json string
             // return the json
+            // .ToList()
+            throw new NotImplementedException();
+        }
+
+        public BsonDocument ExecuteRetrieveFirst(IQueryable<T> retrieveQuery)
+        {
+            // .FirstOrDefault()
             throw new NotImplementedException();
         }
 
