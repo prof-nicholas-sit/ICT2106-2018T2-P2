6f9b98bd2c01d774a66957f2e71653b607d7803b
ernestboey <ernestboeywz@gmail.com>
2018-04-02 03:05:27 +0800

    Added logging decorator for queries

--- SmartHome/DAL/Transactions/MongoDbQueryDecorator.cs
+++ SmartHome/DAL/Transactions/MongoDbQueryDecorator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
@@ -6,18 +7,11 @@ namespace SmartHome.DAL.Transactions
 {
     public abstract class MongoDbQueryDecorator : MongoDbQuery
     {
-        private MongoDbQuery DecoratedMongoDbQuery;
-        
+        protected MongoDbQuery DecoratedMongoDbQuery;
+
         protected MongoDbQueryDecorator(MongoDbQuery mongoDbQuery) : base(mongoDbQuery.FilterDefinition)
         {
             DecoratedMongoDbQuery = mongoDbQuery;
-        }
-
-        public override void Execute()
-        {
-            // This impl can be changed in the future, for now it prints into console some logging information of the 
-            // query being executed
-            Console.WriteLine(String.Format(""));
         }
     }
 }
