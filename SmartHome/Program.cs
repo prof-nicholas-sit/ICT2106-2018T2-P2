c06b77973cf07b7a0c3fd0e4c7d678bd62bcf927
ernestboey <ernestboeywz@gmail.com>
2018-04-02 09:27:06 +0800

    Cleaned up some comments and CRLF issues

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -1,20 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net.Mime;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Driver;
-using SmartHome.DAL.Mappers;
-using SmartHome.Models;
 
 namespace SmartHome
 {
@@ -25,12 +12,7 @@ namespace SmartHome
             Console.WriteLine("Starting Program");
             Console.WriteLine(DateTime.Now.ToString(CultureInfo.CurrentCulture));
 
-//            new AdminMapper().Create(new Administrator() {Username = "test1", Password = "123"}).Save();
-//            Console.WriteLine(new AdminMapper().Login("test1", "123") == null);
-//            Console.WriteLine(new HouseholdMapper().CheckRequestingResetPw("test"));
-            new HouseholdMapper().RequestPasswordReset("test").Save();
-
-//            BuildWebHost(args).Run();
+            BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
