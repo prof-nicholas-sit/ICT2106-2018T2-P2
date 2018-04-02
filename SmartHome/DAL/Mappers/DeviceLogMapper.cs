02daf007c9a1a40c44815337a50312c394d0533d
ernestboey <ernestboeywz@gmail.com>
2018-04-02 08:41:38 +0800

    Added DeviceLog mapper

--- SmartHome/DAL/Mappers/DeviceLogMapper.cs
+++ SmartHome/DAL/Mappers/DeviceLogMapper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using MongoDB.Bson;
+using MongoDB.Driver;
 using SmartHome.DAL.Transactions;
 using SmartHome.Models;
 
@@ -14,25 +15,41 @@ namespace SmartHome.DAL.Mappers
 
         public IDeviceLogMapper CreateLogs(List<DeviceLog> logs)
         {
-            // Similar to Create() but inserts a list of logs
-            // Used when user import a file containing logs
-            throw new NotImplementedException();
+            foreach (DeviceLog deviceLog in logs)
+            {
+                Create(deviceLog);
+            }
+
+            return this;
         }
 
         public List<DeviceLog> SelectFromDateRange(ObjectId householdId, DateTime startDate, DateTime endDate)
         {
-            // make query for selecting deviceapplog within the date range
-            // Uow.ExecuteSelection(query)
-            // return result
-            throw new NotImplementedException();
+            FilterDefinition<BsonDocument> filterDefinition =
+                Builders<BsonDocument>.Filter.Eq("HouseholdId", householdId);
+            filterDefinition &= Builders<BsonDocument>.Filter.Gte("DateTime", startDate);
+            filterDefinition &= Builders<BsonDocument>.Filter.Lte("DateTime", endDate);
+            IEnumerable<BsonDocument> documentList = Uow.ExecuteRetrieveAll(filterDefinition);
+            List<DeviceLog> deviceList = new List<DeviceLog>();
+            foreach (BsonDocument document in documentList)
+            {
+                deviceList.Add(DeserializeDocument<DeviceLog>(document));
+            }
+
+            return deviceList;
         }
 
-        public DeviceLog SelectIndividual(ObjectId householdId, string location, string type, DateTime startDate, DateTime endDate)
+        public DeviceLog SelectIndividual(ObjectId householdId, string location, string type, DateTime startDate,
+            DateTime endDate)
         {
-            // make query for selecting 1 deviceapplog based on all parameters
-            // Uow.ExecuteSelection(query)
-            // return result
-            throw new NotImplementedException();
+            FilterDefinition<BsonDocument> filterDefinition =
+                Builders<BsonDocument>.Filter.Eq("HouseholdId", householdId);
+            filterDefinition &= Builders<BsonDocument>.Filter.Eq("Location", location);
+            filterDefinition &= Builders<BsonDocument>.Filter.Eq("Type", type);
+            filterDefinition &= Builders<BsonDocument>.Filter.Gte("DateTime", startDate);
+            filterDefinition &= Builders<BsonDocument>.Filter.Lte("DateTime", endDate);
+            BsonDocument document = Uow.ExecuteRetrieveFirst(filterDefinition);
+            return DeserializeDocument<DeviceLog>(document);
         }
     }
 }
