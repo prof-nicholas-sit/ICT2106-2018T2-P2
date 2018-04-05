495c556dc223e80e803c88570edae1aceb539ef3
Isfaque <16sic013j@sit.singaporetech.edu.sg>
2018-04-05 13:11:13 +0800

    mongodb connection + output

--- SmartHome/Controllers/HomeController.cs
+++ SmartHome/Controllers/HomeController.cs
@@ -4,12 +4,15 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SmartHome.DAL.Mappers;
 using SmartHome.Models;
 
 namespace SmartHome.Controllers
 {
     public class HomeController : Controller
+        
     {
+        
         public IActionResult Index()
         {
             return View();
@@ -18,6 +21,7 @@ namespace SmartHome.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+        
 
             return View();
         }
--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using MongoDB.Bson;
@@ -10,11 +12,31 @@ namespace SmartHome
 {
     public class Program
     {
+        LinkedList<object> list = new LinkedList<object>();
+        private static IScheduleMapper _ScheduleMapper = new ScheduleMapper();
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Starting Program");
-            Console.WriteLine(DateTime.Now.ToString(CultureInfo.CurrentCulture));
+
+        Console.WriteLine("Starting Program");
+            //Console.WriteLine(DateTime.Now.ToString(CultureInfo.CurrentCulture));
+            List<String> list1 = new List<string>();
+
+            list1.Add("1");
+            list1.Add("2");
+
+            Schedule lol = new Schedule();
+            lol.StartTime = new DateTime();
+            lol.EndTime = new DateTime();
+            lol.ApplyToEveryWeek = true;
+            lol.DayOfWeek = list1;
+            _ScheduleMapper.Create(lol).Save().Commit();
             
+            
+            Console.WriteLine(_ScheduleMapper.SelectAll() + "\n");
+
+            //Console.WriteLine(_ScheduleMapper.SelectById("sdfsdf"));
+
             BuildWebHost(args).Run();
         }
 
