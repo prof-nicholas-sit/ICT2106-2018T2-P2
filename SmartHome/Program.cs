a718b6d0f092154bdce8f705a9ac9a3b799e96c7
Isfaque <16sic013j@sit.singaporetech.edu.sg>
2018-04-05 16:53:39 +0800

    mongo access

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using MongoDB.Bson;
+using MongoDB.Driver.Builders;
 using SmartHome.DAL.Mappers;
 using SmartHome.Models;
 
@@ -18,22 +19,28 @@ namespace SmartHome
         public static void Main(string[] args)
         {
 
-        Console.WriteLine("Starting Program");
+            Console.WriteLine("Starting Program");
             //Console.WriteLine(DateTime.Now.ToString(CultureInfo.CurrentCulture));
             List<String> list1 = new List<string>();
 
             list1.Add("1");
             list1.Add("2");
 
+
+
             Schedule lol = new Schedule();
             lol.StartTime = new DateTime();
             lol.EndTime = new DateTime();
             lol.ApplyToEveryWeek = true;
             lol.DayOfWeek = list1;
-            _ScheduleMapper.Create(lol).Save().Commit();
+            lol.test = "SDfsd";
+
+            ObjectId id = new ObjectId("5ac5c582327aa3469c7621db");
             
+           // _ScheduleMapper.SelectByDevice(id);
+            //_ScheduleMapper.Create(lol).Save().Commit();
             
-            Console.WriteLine(_ScheduleMapper.SelectAll() + "\n");
+            Console.WriteLine(_ScheduleMapper.SelectById(id).EndTime + "\n");
 
             //Console.WriteLine(_ScheduleMapper.SelectById("sdfsdf"));
 
