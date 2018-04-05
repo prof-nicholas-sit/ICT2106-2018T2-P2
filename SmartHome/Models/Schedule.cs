8d7bf67dd4a4a6484d4be3d83759700eab48369a
jeremyteh <magnificentbluez@hotmail.com>
2018-04-05 12:25:05 +0800

    Revert "Merge branch 'Team15' into Team15-dev"
    This reverts commit 0785a690351a0b6989c67c2a56971437b15b87ee.

--- SmartHome/Models/Schedule.cs
+++ SmartHome/Models/Schedule.cs
@@ -15,18 +15,19 @@ namespace SmartHome.Models
 
         [Required]
         [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
-        public TimeSpan startTime { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        public DateTime startTime { get; set; }
 
         [Required]
         [DataType(DataType.Time)]
-        [DisplayFormat(DataFormatString = "{0:hh\\:mm}", ApplyFormatInEditMode = true)]
-        public TimeSpan endTime { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        public DateTime endTime { get; set; }
 
         public bool applyToEveryWeek { get; set; }
 
         public string dayOfWeek { get; set; }
-                
+
+        
         [ForeignKey("device")]
         public int deviceId { get; set; }
         
