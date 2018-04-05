38cc6e448048c6fc48d57aba930cd15df7099d43
seanyow <31947297+seanyow@users.noreply.github.com>
2018-04-05 16:50:50 +0800

    mongo changes
    eee

--- SmartHome/Models/DeviceLogFactory.cs
+++ SmartHome/Models/DeviceLogFactory.cs
@@ -18,22 +18,25 @@ namespace SmartHome.Models
         //public void CreateClass(string[] data) // Temporary Console.log 
         {
             DeviceLog newDevice = null;
-            switch (data[4].ToLower())
+            switch (data[2].ToLower())
             {
                 case "aircon":
                     // Temporary hardcode, future improvement can check for a header in the excel file. Can do if there is time....
                     newDevice = new AirconDeviceLog("5349b4ddd2781d08c09890f3", data[0], data[1], data[2], data[3], Convert.ToDouble(data[4]), DateTime.Parse(data[5]),
                         Convert.ToInt32(data[6]), data[7], data[8], data[9]);
+                    //return newDevice;
                     //System.Diagnostics.Debug.WriteLine("AIRCON:\n" + newAC);
                     break;
                 case "fan":
                     newDevice = new FanDeviceLog("5349b4ddd2781d08c09890f3", data[0], data[1], data[2], data[3], Convert.ToDouble(data[4]), DateTime.Parse(data[5]),
                         Convert.ToInt32(data[6]));
+                    //return newDevice;
                     //System.Diagnostics.Debug.WriteLine("FAN:\n" + newFan);
                     break;
                 case "light":
                     newDevice = new LightDeviceLog("5349b4ddd2781d08c09890f3", data[0], data[1], data[2], data[3], Convert.ToDouble(data[4]), DateTime.Parse(data[5]),
                         Convert.ToInt32(data[6]), Convert.ToInt32(data[7]));
+                    //return newDevice;
                     //System.Diagnostics.Debug.WriteLine("LIGHT:\n" + newLight);
                     break;
                 default:
