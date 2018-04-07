9f708cb331d034dcb0fd9aa454ca350b6acd67a9
ctjsctjs <ctjsctjs@gmail.com>
2018-04-07 23:49:47 +0800

    Work in progress

--- SmartHome/Models/UsageStatistics/EnergyAdvise.cs
+++ SmartHome/Models/UsageStatistics/EnergyAdvise.cs
@@ -11,15 +11,18 @@ namespace UsageStatistics.Models
     public class EnergyAdvise
     {
         public double HouseholdConsumption { get { return new EnergyUsage().TotalEnergyUsage(); } }
-        public double AverageConsumption { get { return CalculateAverageConsumption(); } }
+        //public double AverageConsumption { get { return CalculateAverageConsumption(); } }
         public double PreviousMonthConsumption { get { return GetPreviousMonthConsumption(); } }
-
+        public string GraphData { get { return GenerateGraphData(); } }
         private Session _session;
         private List<Household> allHouseholds = new List<Household>();
         private List<DeviceLog> allDeviceLogs = new List<DeviceLog>();
         private DateTime firstDayOfMonth;
         private DateTime lastDayOfMonth;
-        public string month { get; set; }
+        private DateTime firstWeekOfMonth;
+        private DateTime secondWeekOfMonth;
+        private DateTime thirdWeekOfMonth;
+        private DateTime fourthWeekOfMonth;
 
         public EnergyAdvise(int month)
         {
@@ -31,18 +34,30 @@ namespace UsageStatistics.Models
             DateTime date = new DateTime(DateTime.Now.Year, month, DateTime.Now.Day);
             firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
             lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+
+            //Get end day of each week
+            firstWeekOfMonth = firstDayOfMonth.AddDays(6);
+            secondWeekOfMonth = firstWeekOfMonth.AddDays(7);
+            thirdWeekOfMonth = secondWeekOfMonth.AddDays(7);
+            fourthWeekOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
         }
 
-        private double CalculateAverageConsumption()
+        private double CalculateAverageConsumption(DateTime startDate, DateTime endDate)
         {
             double sum = 0;            
             
+            //Get all device fromeach household
             foreach (Household household in allHouseholds)
             {
-                allDeviceLogs.AddRange(new DeviceLogMapper().SelectFromDateRange(household.houseHoldId, firstDayOfMonth, lastDayOfMonth));                
+                System.Diagnostics.Debug.WriteLine("HouseholdId: " + household.houseHoldId);
+                System.Diagnostics.Debug.WriteLine("startDate: " + startDate);
+                System.Diagnostics.Debug.WriteLine("endDate: " + endDate);
+
+                allDeviceLogs.AddRange(new DeviceLogMapper().SelectFromDateRange(household.houseHoldId, startDate, endDate));                
                 System.Diagnostics.Debug.WriteLine("Instantiated how many: " + allDeviceLogs.Count);
             }
 
+            //Sum all energy usage for all devices from all household
             if (allDeviceLogs.Count != 0)
             {
                 foreach (DeviceLog log in allDeviceLogs)
@@ -50,8 +65,8 @@ namespace UsageStatistics.Models
                     sum += new EnergyUsage().TotalEnergyUsage();
                 }
             }
-            
-            return sum/allHouseholds.Count;
+
+            return sum /allHouseholds.Count;
         }
 
         private double GetPreviousMonthConsumption()
@@ -87,6 +102,37 @@ namespace UsageStatistics.Models
             }
 
             return sum;
+        }
+        private string GenerateGraphData()
+        {
+            //Init data list
+            List<double> myHouseholdData = new List<double>();
+            List<double> averageHouseholdData = new List<double>();
+            List<double> previousMonthData = new List<double>();
+
+            //Algorithm to pull data from DB
+            myHouseholdData.Add(5);
+            myHouseholdData.Add(15);
+            myHouseholdData.Add(55);
+            myHouseholdData.Add(25);
+
+            averageHouseholdData.Add(CalculateAverageConsumption(firstDayOfMonth, firstWeekOfMonth));
+            averageHouseholdData.Add(CalculateAverageConsumption(firstWeekOfMonth, secondWeekOfMonth));
+            averageHouseholdData.Add(CalculateAverageConsumption(secondWeekOfMonth, thirdWeekOfMonth));
+            averageHouseholdData.Add(CalculateAverageConsumption(thirdWeekOfMonth, lastDayOfMonth));
+
+            previousMonthData.Add(55);
+            previousMonthData.Add(33);
+            previousMonthData.Add(22);
+            previousMonthData.Add(77);
+
+            //Parse to JSON format
+            String myHouseholdJSON = string.Join(",", myHouseholdData);
+            String averageHouseholdJSON = string.Join(",", averageHouseholdData);
+            String previousMonthJSON = string.Join(",", previousMonthData);
+            String output = "[" + "[" + myHouseholdJSON + "]," + "[" + averageHouseholdJSON + "]," + "[" + previousMonthJSON + "]" + "]";
+
+            return output;
         }
     }
 }
